263e65e9e44773be3537a1cc6d89fe5b92af577f
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2024-08-24 19:12:02 +0700

    almost messed shit up lol

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends Control
 
-var includeUtils = preload("res://utils.gd")
+var includeUtils = preload("res://scripts/utils.gd")
 var utils = includeUtils.new()
 @onready var playerX_percentage = 0
 var inp = ""
@@ -8,23 +8,27 @@ var locked = 0
 @export var keyINCR: Key
 @export var keyDCR: Key
 @export var keyLOCK: Key
+@export var timer: Timer
 @onready var panel = $"."
 @onready var panel_label = panel.get_child(0)
-var isHovering_1 = false
-var isHovering_2 = false
-var styleBox: StyleBoxFlat = get_theme_stylebox("panel").duplicate()
+@onready var panel_label_sprite = panel.get_child(1)
+var bottom_pos = utils.bottom_bar_pos_y
+var top_pos = utils.bottom_bar_pos_y
+
 func _ready():
-	pass
+	print(panel.get_child(1))
+	timer.start(10)
 
 func _process(delta):
-	inp = int(playerX_percentage)
-	panel_label.text = str(inp)+"%"
+	panel_label.text = str(int(playerX_percentage))+"%"
 	if not locked:
 		if Input.is_key_pressed(keyINCR):playerX_percentage += 0.34
 		if Input.is_key_pressed(keyDCR): playerX_percentage -= 0.34
 		playerX_percentage = clamp(playerX_percentage,0,100)
-		panel.position.y = utils.percentage_to_posY(39,480,playerX_percentage)
-	if Input.is_key_pressed(keyLOCK):
-		add_theme_stylebox_override("panel",styleBox)
+		panel.position.y = utils.percentage_to_posY(30,440,playerX_percentage)
+	if Input.is_key_pressed(keyLOCK) or timer.time_left == 0:
+		timer.stop()
+		panel_label_sprite.texture = load("res://assets/rightbox.png")
 		locked = 1
-		styleBox.set("bg_color",Color.RED)
+func _on_counting_down_b_4_ded_timeout():
+	pass
