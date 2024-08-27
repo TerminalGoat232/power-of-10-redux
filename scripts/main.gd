bc7db40491636fe47560e09446a338b39a16b4c8
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2024-08-27 23:56:17 +0700

    disgusting

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,6 +4,7 @@ var utils = includeUtils.new()
 var input_box_texture = load("res://assets/inputbox.png")
 
 @export var n_walk_til_awnser = 4
+
 var player1_percentage = 0
 var player2_percentage = 0
 
@@ -13,6 +14,7 @@ var rd_walk_til_answer = []
 var orig_pos = utils.bottom_bar_pos_y
 var max_pos  = utils.top_bar_pos_y
 var onNextAnswer = 0
+
 var question_n = 0
 var round = 0
 var w_counted = 0
@@ -23,7 +25,7 @@ var qna_json_file = "res://qna.json"
 var qna_dict = qna_info()
 var qna_round = [qna_dict["ROUND_1"],qna_dict["ROUND_2"]]
 
-@onready var timer_ticking_sound = $"../timer_ticking"
+@onready var timer_ticking_sound = $"../timer_end"
 @onready var main_anim = $main_anim
 signal dumb_sig
 var tts_stopped = 1
@@ -41,6 +43,7 @@ func qna_info():
 func decide_who_wins():
 	var mismatch_p1 = abs(player1_percentage-qna_round[round][question_n]["answer"])
 	var mismatch_p2 = abs(player2_percentage-qna_round[round][question_n]["answer"])
+	print(question_n," ", mismatch_p1," ", mismatch_p2," ",qna_round[round][question_n]["answer"])
 	if mismatch_p1 < mismatch_p2:
 		utils.player_1_pts += 1
 		$tickboxes_cnt_1.get_child(utils.player_1_pts-1).show()
@@ -49,6 +52,7 @@ func decide_who_wins():
 		utils.player_2_pts += 1
 		$tickboxes_cnt_2.get_child(utils.player_2_pts-1).show()
 		print("player 2 wins")	
+	question_n += 1
 func show_result():
 	var pos = utils.percentage_to_val(max_pos,orig_pos,rd_walk_til_answer[w_counted])
 	var perc = round(utils.val_to_percentage(max_pos,orig_pos,$panel_3.position.y)*100)
@@ -57,9 +61,8 @@ func show_result():
 	$panel_3/res_lbl.text = str(perc)+"%"
 	if (w_counted <= n_walk_til_awnser-1) and (perc == rd_walk_til_answer[w_counted]): w_counted+=1
 	elif (w_counted == n_walk_til_awnser) and (perc == qna_round[round][question_n]["answer"]):
-
 		main_anim.play("tick_box_for_player")
-		question_n += 1
+
 		onShowResult = 0
 		set_process(!is_processing())
 		
@@ -73,6 +76,7 @@ func _process(delta):
 		
 		if tts_stopped:
 			tts_stopped=0
+			main_anim.stop()
 			timer_ticking_sound.play(10.0)
 			
 	if onShowResult:
@@ -84,13 +88,12 @@ func _process(delta):
 		if (int($panel_3.position.y) == orig_pos-1) and (int(player1_percentage) == 0):
 			dumb_sig.emit()
 			onNextAnswer=0
+	timer_ticking()
+func timer_ticking():
 	if !$panel_1/timer_1.is_stopped():
 		$timer_panel.get_child(int(11-$panel_1/timer_1.time_left)).hide()
-	
 	if !$panel_2/timer_2.is_stopped():
 		$timer_panel2.get_child(int(11-$panel_2/timer_2.time_left)).hide()
-func signal_per_sec():
-	pass
 func resetPanels(delta):
 		
 	$panel_1.playerX_percentage = lerpf(player1_percentage,0.0,delta*3)
@@ -101,6 +104,8 @@ func resetPanels(delta):
 	$panel_3/res_lbl.text = str(perc)+"%"
 	$panel_1/input_box_sprite_p1.texture = input_box_texture
 	$panel_2/input_box_sprite_p1.texture = input_box_texture
+
+func show_timer():
 	$timer_panel.show()
 	$timer_panel2.show()
 	for z in range(0,10):
@@ -122,11 +127,17 @@ func _on_next_pressed():
 
 
 func _on_start_timer_pressed():
-	
-	timer_ticking_sound.play()
-	$panel_1/timer_1.start()
-	$panel_2/timer_2.start()
+	set_process(!is_processing())
+	main_anim.play("timer_ticking")
+	#timer_ticking_sound.play()
 	tts_stopped = 1
 	
 func _on_ans_pressed():
 	main_anim.play("answer_holding")
+	
+func start_timer():
+	$panel_1/timer_1.start()
+	$panel_2/timer_2.start()
+	
+func _stop_proc():
+	set_process(!is_processing())
