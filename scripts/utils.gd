7b6d308f0982e8673e1dc3e3e906606b4e078c20
TerminalGoat <74200268+TerminalGoat232@users.noreply.github.com>
2024-08-31 12:03:09 +0700

    bl

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -5,7 +5,7 @@ var top_secondary_bar_scl = 0.52
 var bottom_secondary_bar_scl = 0.3
 var player_1_pts = 0
 var player_2_pts = 0
-var not_locked_first = 0
+
 func percentage_to_val(max,min,percentage):
 	return (max-min)*(percentage)/100 + min
 func val_to_percentage(max,min,y):
